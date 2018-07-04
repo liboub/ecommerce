650c17541750a48a1984e8efc7f695e2f1046e2f
liboub <a.libouban@laposte.net>
2018-07-04 15:55:05 +0200

    changement accuei 79

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AccueilController extends Controller
 {
-    //
+    public function view()
+    {
+     $composants = DB::table('composants')->get();
+     return view('pages.accueil', compact('composants'));
+    }
 }
