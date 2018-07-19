4ce635ad5484de88ce5922ba4c776c8ff7a61122
liboub <a.libouban@laposte.net>
2018-07-19 13:12:02 +0200

    cart storage ok +mise en page

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -29,7 +29,7 @@
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <form action="#">
                         <div class="table-content table-responsive">
-                            <h2>Processeur</h2>
+                            <h2>votre panier</h2>
                             <table>
                                 <thead>
                                 <tr>
@@ -58,15 +58,9 @@
                         <div class="row">
                             <div class="col-md-8 col-sm-7 col-xs-12">
                                 <div class="buttons-cart">
-                                    <input type="submit" value="Update Cart" />
                                     <a href="/shop">Continue Shopping</a>
                                 </div>
-                                <div class="coupon">
-                                    <h3>Coupon</h3>
-                                    <p>Enter your coupon code if you have one.</p>
-                                    <input type="text" placeholder="Coupon code" />
-                                    <input type="submit" value="Apply Coupon" />
-                                </div>
+
                             </div>
                             <div class="col-md-4 col-sm-5 col-xs-12">
                                 <div class="cart_totals">
@@ -84,18 +78,18 @@
                                                     <li>
                                                         <input type="radio" />
                                                         <label>
-                                                            Flat Rate: <span class="amount">£7.00</span>
+                                                            frais de livraison: <span class="amount">£7.00</span>
                                                         </label>
                                                     </li>
                                                     <li>
                                                         <input type="radio" />
                                                         <label>
-                                                            Free Shipping
+                                                            frais de livraison gratuits
                                                         </label>
                                                     </li>
                                                     <li></li>
                                                 </ul>
-                                                <p><a class="shipping-calculator-button" href="#">Calculate Shipping</a></p>
+                                                <p><a class="shipping-calculator-button" href="#">calculer les frais d'envois</a></p>
                                             </td>
                                         </tr>
                                         <tr class="order-total">
